588350aab62c7bb4edb50a7c6b13233e5d08d051
Michal Ziulek <michalziulek@gmail.com>
2023-12-03 11:21:33 +0100

    Use 'zig cc' to build example BOF runner.

--- build.bat
+++ build.bat
@@ -1,6 +1,14 @@
-@REM `bof-launcher` library is not compatible with `__declspec(dllimport)`
-@REM so we define `DECLSPEC_IMPORT` to be empty.
-@REM `-lc` is needed for Zig to find `windows.h`, BOF is *not* linked with libc.
+@REM STEP 1.
+@REM Build BOF.
+@REM
+@REM `bof-launcher` library is not compatible with `__declspec(dllimport)` so we define
+@REM `DECLSPEC_IMPORT` to be empty.
+@REM `-lc` is needed for Zig to find `windows.h`, BOF is *not* linked with libc (but can still use it).
+@REM
 zig build-obj -O ReleaseSmall -lc -D"DECLSPEC_IMPORT=" example_bof.c
 
-zig build-exe -O ReleaseSmall -lc example_bof_runner.c bof-launcher_win_x64.lib ole32.lib ws2_32.lib
+@REM STEP 2.
+@REM Build your application with `zig cc` (which is basically a standalone, zero-dependency `clang` with
+@REM cross-compilation working out-of-the-box).
+@REM
+zig cc -lc -o example_bof_runner.exe example_bof_runner.c bof-launcher_win_x64.lib ole32.lib ws2_32.lib
